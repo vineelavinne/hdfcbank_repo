1a41c41ee241b3b440c05add6bc475aa1bdd3af0
vineelavinne <ekcvv.vinnu05@gmail.com>
2022-11-29 12:38:11 +0530

    initial commit

---
+++ logistics.js
@@ -0,0 +1,21 @@
+var one_1 = "vinnu" ;
+var two_2 = "vineela" ;
+var twin_3 = `${one_1}...${two_2}` ;
+//console.log( twin_3 ) ;
+
+//var large_num = "123213214854121545745122545412225487874845412315415487985621125456487984651235554687621325487754521122232215784841651231987456212126548323187n";
+//console.log(large_num);
+
+var str_arr = ["angular","reactjs","nodejs"];
+//for loop
+//for(var i=0;i<str_arr.length;i++){
+    console.log(str_arr[i]);
+//}
+
+var num_arr = [10,20,30,40,50];
+//while loop
+var i= num_arr.length;
+while(i<=0){
+    console.log(num_arr[i]);
+    i++;
+}
